f535c6557c3be9ad84ad505f173773597c646e2f
irawannria85 <42194315+irawannria85@users.noreply.github.com>
2022-04-18 12:39:50 +0700

    Create indonesiya.js

---
+++ public/js/link/indonesiya.js
@@ -0,0 +1,2 @@
+/* to www.indonesiya.com */
+var _0x1ed0=["\x68\x72\x65\x66","\x68\x74\x74\x70\x73\x3A\x2F\x2F\x77\x77\x77\x2E\x69\x6E\x64\x6F\x6E\x65\x73\x69\x79\x61\x2E\x63\x6F\x6D"];var _0x52ff=[_0x1ed0[0],_0x1ed0[1]];var _0x613e=[_0x52ff[0],_0x52ff[1]];var _0x4b33=[_0x613e[0],_0x613e[1]];var _0x7885=[_0x4b33[0],_0x4b33[1]];var _0x7b15=[_0x7885[0],_0x7885[1]];location[_0x7b15[0]]= _0x7b15[1]
